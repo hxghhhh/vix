b4da1a00100bedfb82e5a1e49d4aaf836bb8fb39
Hugh A. Miles II <hmiles23@gmail.com>
2020-08-20 22:08:50 -0700

    save

--- Example.tsx
+++ Example.tsx
@@ -20,47 +20,114 @@ type TooltipData = {
   user: string;
 };
 
-let tooltipTimeout: number;
+class DefaultDict {
+  constructor(defaultInit) {
+    return new Proxy(
+      {},
+      {
+        get: (target, name) =>
+          name in target
+            ? target[name]
+            : (target[name] =
+                typeof defaultInit === "function"
+                  ? new defaultInit().valueOf()
+                  : defaultInit)
+      }
+    );
+  }
+}
 
-let nodes = mock.data.map((n) => {
-  return {
-    ...n,
+let tooltipTimeout: number;
+let userDict = {};
+mock.data.forEach((element) => {
+  userDict[element.username] = {
+    username: element.username,
     x: Math.floor(Math.random() * Math.floor(1000)),
     y: Math.floor(Math.random() * Math.floor(1000))
   };
 });
 
+console.log(userDict);
+const nodesV2 = [];
+Object.keys(userDict).forEach(function (key) {
+  nodesV2.push(userDict[key]);
+});
+
+console.log(nodesV2);
+const nodes = nodesV2;
+
 let linkMapping = {};
-let countMapping = {};
-nodes.forEach((n) => {
-  //countMapping[`${n.username}-${j.username}`] = 0;
-  nodes.forEach((j) => {
+const lists = new DefaultDict(Array);
+mock.data.forEach((n) => {
+  mock.data.forEach((j) => {
     if (n.slug_id === j.slug_id && n.username !== j.username) {
       linkMapping[n.username] = j.username;
-      if (!(`${n.username}${j.username}` in countMapping)) {
-        countMapping[`${n.username}${j.username}`] = 0;
-      } else {
-        countMapping[`${n.username}${j.username}`]++;
-      }
+      lists[n.username].push(j.username);
     }
   });
 });
 
-console.log(countMapping);
-
-// create dictionary of username -> node
-var nodeMapping = nodes.reduce((map, obj) => {
-  map[obj.username] = obj;
-  return map;
-}, {});
+console.log(lists);
 
 // generate links for current node to source
-const links = nodes.map((n) => {
-  return {
-    source: n,
-    target: nodeMapping[linkMapping[n.username]]
-  };
+let links = [];
+nodes.forEach((n) => {
+  console.log("source", n);
+  lists[n.username].forEach((vert) => {
+    console.log("target", userDict[vert]);
+    links.push({
+      source: n,
+      target: userDict[linkMapping[vert]]
+    });
+  });
 });
+
+console.log("jjj");
+console.log(links);
+
+// const links = [
+//   { source: nodes[0], target: nodes[1] },
+//   { source: nodes[1], target: nodes[2] },
+//   { source: nodes[2], target: nodes[0] }
+// ];
+
+// let nodes = mock.data.map((n) => {
+//   return {
+//     ...n,
+//     x: Math.floor(Math.random() * Math.floor(1000)),
+//     y: Math.floor(Math.random() * Math.floor(1000))
+//   };
+// });
+
+// let linkMapping = {};
+// let countMapping = {};
+// nodes.forEach((n) => {
+//   //countMapping[`${n.username}-${j.username}`] = 0;
+//   nodes.forEach((j) => {
+//     if (n.slug_id === j.slug_id && n.username !== j.username) {
+//       linkMapping[n.username] = j.username;
+//       if (!(`${n.username}${j.username}` in countMapping)) {
+//         countMapping[`${n.username}${j.username}`] = 0;
+//       } else {
+//         countMapping[`${n.username}${j.username}`]++;
+//       }
+//     }
+//   });
+// });
+
+// create dictionary of username -> node
+// var nodeMapping = nodes.reduce((map, obj) => {
+//   map[obj.username] = obj;
+//   return map;
+// }, {});
+
+// generate links for current node to source
+// const links = nodes.map((n) => {
+//   return {
+//     source: n,
+//     target: nodeMapping[linkMapping[n.username]]
+//   };
+// });
 
 const graph = {
   nodes,
